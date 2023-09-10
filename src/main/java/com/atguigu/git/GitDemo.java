d78300b6633d9da5453c9bcdd074daa217e46642
Lixiang <2358018791@qq.com>
2023-09-10 10:21:24 +0800

    second commit

--- src/main/java/com/atguigu/git/GitDemo.java
+++ src/main/java/com/atguigu/git/GitDemo.java
@@ -3,5 +3,6 @@ package com.atguigu.git;
 public class GitDemo {
     public static void main(String[] args) {
         System.out.println("Hello git!");
+        System.out.println("hello git2!");
     }
 }
